301e0825b5bd7baf7f0e5a17b273134c775e7a4a
yoshapurohit <yoshapurohit@gmail.com>
2026-09-16 10:03:00 +0530

    Add files via upload

---
+++ q35.c
@@ -0,0 +1,28 @@
+//Q35: Write a program to print all factors of a given number.
+
+/*
+Sample Test Cases:
+Input 1:
+6
+Output 1:
+1 2 3 6
+
+Input 2:
+10
+Output 2:
+1 2 5 10
+
+*/
+#include <stdio.h>
+int main(){
+	int num,i;
+	printf("Enter a number : ");
+	scanf("%d",&num);
+	for (i=num;i>0;i--){
+		if (num%i==0){
+			printf("%d, ",i);
+			continue;
+		}
+	}
+	return 0;
+}
