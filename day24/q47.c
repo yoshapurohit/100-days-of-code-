85e9c681b139fb83ac14b16419c81bdf197015e8
yoshapurohit <yoshapurohit@gmail.com>
2026-09-19 22:27:40 +0530

    Add files via upload

---
+++ day24/q47.c
@@ -0,0 +1,33 @@
+/*Q47: Write a program to print the following pattern:
+*
+**
+***
+****
+*****
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*
+**
+***
+****
+*****
+
+*/
+#include <stdio.h>
+
+int main(){
+	int i,j,n;
+	printf("enter the value of n : ");
+	scanf("%d",&n);
+	for (i=1;i<=n;i++){
+		for (j=1;j<=i;j++){
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
---
+++ day24/q48.c
@@ -0,0 +1,33 @@
+/*Q48: Write a program to print the following pattern:
+1
+12
+123
+1234
+12345
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+1
+12
+123
+1234
+12345
+
+*/
+#include <stdio.h>
+
+int main(){
+	int i,j,n;
+	printf("enter the value of n : ");
+	scanf("%d",&n);
+	for (i=1;i<=n;i++){
+		for (j=1;j<=i;j++){
+			printf("%d",j);
+		}
+		printf("\n");
+	}
+	return 0;
+}
